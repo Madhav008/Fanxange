5e5ec854aeb0b2c097014fb59a6aa85df634b46e
Madhav008 <madhavj211@gmail.com>
2023-05-17 17:05:37 +0530

    Current Performace points is done

--- Points.table.js
+++ Points.table.js
@@ -8,51 +8,56 @@ let player_past_performance = {
 }
 
 
-let player_current_performace = {
-    singles:'',
-    sixes: '',
-    fours: '',
-    out:'',
-    hundred:'',
-    fifty:'',
-    duck:'',
-    catches_taken:'',
-    stumping:'',
-    run_out:'',
-    direct_hit_out:'',
-    overthrow:'',
-    wickets:'',
-    median_over:'',
-    hatrik:'',
+let player_current_performance = {
+    singles:39,
+    sixes: 5,
+    fours: 2,
+    out:1,
+    hundred:1,
+    fifty:2,
+    duck:0,
+    catches_taken:1,
+    stumping:1,
+    run_out:1,
+    direct_hit_out:0,
+    overthrow:2,
+    wickets:0,
+    median_over:0,
+    hatrik:0,
 }
 //Strike rate and Economy rate not be included in this as this decide the current performance of the player
 let player_points = {
-    singles:1,
+    singles: 1,
     sixes: 2,
     fours: 1,
-    out:-1,
-    hundred:16,
-    fifty:8,
-    duck:-2,
-    catches_taken:8,
-    run_out:6,
-    stumping:12,
-    direct_hit_out:12,
-    overthrow:-2,
-    wickets:25,
-    median_over:12,
-    hatrik:4,
-}
+    out: -1,
+    hundred: 16,
+    fifty: 8,
+    duck: -2,
+    catches_taken: 8,
+    run_out: 6,
+    stumping: 12,
+    direct_hit_out: 12,
+    overthrow: -2,
+    wickets: 25,
+    median_over: 12,
+    hatrik: 4,
+};
 
+function getCurrentPerformacePoints() {
+    let points = 0;
 
-function getCurrentPerformacePoints(player_current_performace){
-    let points;
-
-
+    // Check if each key exists in player_current_performance and has a non-zero value
+    for (let key in player_points) {
+        if (key in player_current_performance && player_current_performance[key] !== 0) {
+            points += player_current_performance[key] * player_points[key];
+        }
+    }
 
     return points;
 }
 
+console.log(getCurrentPerformacePoints())
 
 function getPastPerformacePoints(player_past_performance){
     let points;
