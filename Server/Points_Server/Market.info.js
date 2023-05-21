c16ac80c6f694a8e48fe1f1a324a977a4b7ad9b2
Madhav008 <madhavj211@gmail.com>
2023-05-21 14:11:33 +0530

    Filter the Unecceasry Data

--- Server/Points_Server/Market.info.js
+++ Server/Points_Server/Market.info.js
@@ -34,15 +34,17 @@ function getPlayerData() {
                 console.log("Teams:", player_stats.data.sixergame_sixers[0].allTeams);
             })
 
-            // getPlayerStats(player.cricviz_id).then((player_stats) => {
+            getPlayerStats(player.cricviz_id).then((player_stats) => {
+                getPlayerMatchStats(player_stats)
+                // fs.writeFile(`${player.symbol}.json`, JSON.stringify(player_stats), (err) => {
+                //     if (err) {
+                //         console.error('Error writing JSON file:', err);
+                //     } else {
+                //         console.log('JSON file has been written successfully.');
+                //     }
 
-            //     fs.writeFile(`${player.symbol}.json`, JSON.stringify(player_stats), (err) => {
-            //         if (err) {
-            //             console.error('Error writing JSON file:', err);
-            //         } else {
-            //             console.log('JSON file has been written successfully.');
-            //         }
-            //     });
+
+                });
 
 
 
@@ -53,25 +55,46 @@ function getPlayerData() {
 }
 getPlayerData()
 
-const PlayerStats = require('./v_kohli.json');
 const getMatchStats = require('./Match.stats');
 
-function getPlayerMatchStats() {
-    PlayerStats.data.datascience_cricviz_player_match.forEach(match => {
+function getPlayerMatchStats(player_stats) {
+    player_stats.data.datascience_cricviz_player_match.forEach(match => {
         console.log("Start Time:", match.start_timestamp);
-        console.log("Start Time:", match.match_scoring.sixer_player_match_id);
+        console.log("Match Id:", match.match_scoring.sixer_player_match_id);
         // console.log("Start Time:", match.start_timestamp);
         getMatchStats(match.match_scoring.sixer_player_match_id).then((player_stats) => {
 
-            fs.writeFile(`${match.match_scoring.match}.json`, JSON.stringify(player_stats), (err) => {
-                if (err) {
-                    console.error('Error writing JSON file:', err);
-                } else {
-                    console.log('JSON file has been written successfully.');
-                }
-            });
+            // fs.writeFile(`${match.match_scoring.match}.json`, JSON.stringify(player_stats), (err) => {
+            //     if (err) {
+            //         console.error('Error writing JSON file:', err);
+            //     } else {
+            //         console.log('JSON file has been written successfully.');
+            //     }
+            // });
 
+            
+            let player_current_performance = {
+                "bat_runs": player_stats.data.datascience_cricviz_match_scoring[0].bat_runs,
+                "balls_faced": player_stats.data.datascience_cricviz_match_scoring[0].balls_faced,
+                "fours_hit": player_stats.data.datascience_cricviz_match_scoring[0].fours_hit,
+                "sixes_hit": player_stats.data.datascience_cricviz_match_scoring[0].sixes_hit,
+                "bowl_wickets": player_stats.data.datascience_cricviz_match_scoring[0].bowl_wickets,
+                "balls_bowled": player_stats.data.datascience_cricviz_match_scoring[0].balls_bowled,
+                "bowl_runs": player_stats.data.datascience_cricviz_match_scoring[0].bowl_runs,
+                "dot_balls": player_stats.data.datascience_cricviz_match_scoring[0].dot_balls,
+                "maidens": player_stats.data.datascience_cricviz_match_scoring[0].maidens,
+                "catch": player_stats.data.datascience_cricviz_match_scoring[0].catch,
+                "stumping": player_stats.data.datascience_cricviz_match_scoring[0].stumping,
+                "run_out": player_stats.data.datascience_cricviz_match_scoring[0].run_out,
+                "run_points": player_stats.data.datascience_cricviz_match_scoring[0].run_points,
+                "milestone_bonus": player_stats.data.datascience_cricviz_match_scoring[0].milestone_bonus,
+                "batting_participation_points": player_stats.data.datascience_cricviz_match_scoring[0].batting_participation_points,
+                "batting_multiplier":player_stats.data.datascience_cricviz_match_scoring[0].batting_multiplier,
+                "match":match.match_scoring.match,
+                "Date":match.start_timestamp
+              };
 
+            console.log(player_current_performance)
 
         })
     });
