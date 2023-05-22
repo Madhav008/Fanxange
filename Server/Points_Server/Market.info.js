236a7f404aeeeec04a2d413bb25517937e3f35b0
Madhav008 <madhavj211@gmail.com>
2023-05-22 15:28:58 +0530

    Player iNfo added

--- Server/Points_Server/Market.info.js
+++ Server/Points_Server/Market.info.js
@@ -1,8 +1,13 @@
 const getPlayerStats = require('./Player.info');
 const getCrickvizPlayer = require('./Cricviz.player');
 
+const getMatchStats = require('./Match.stats');
+const { calculateBattingPoints, calculateFieldingPoints } = require('./Points.table');
+const getPlayerPoints = require('./Points.table');
+
 var MarketData = require('./market-info.json')
-const fs = require('fs')
+const fs = require('fs');
+const insertData = require('./SeedData/Seed.Playerinfo');
 
 MarketData = JSON.stringify(MarketData)
 // Parse JSON
@@ -12,135 +17,119 @@ const data = JSON.parse(MarketData);
 
 
 
-function getPlayerData() {
-    const players = data.data.players;
-    Object.keys(players).forEach(playerKey => {
-        const player = players[playerKey];
+async function getPlayerData() {
+  const players = data.data.players;
+  Object.keys(players).forEach(playerKey => {
+    const player = players[playerKey];
 
-        if (player.name == "Jasprit Bumrah") {
-            getCrickvizPlayer(player.symbol, player.cricviz_id).then((player_stats) => {
-                let player_info = {
-                    "Player": playerKey,
-                    "Name": player.name,
-                    "Player Type": player.playerType,
-                    "Tournaments": player.tournaments,
-                    "Jersy Number": player.jersey_number,
-                    "Sixer ID": player.sixer_id,
-                    "Cricviz ID": player.cricviz_id,
-                    "Sport ID": player.sportId,
-                    "Symbol": player.symbol,
-                    "Teams": player_stats.data.sixergame_sixers[0].allTeams
-                }
-                console.log(player_info)
-            })
+    // if (player.name == "V Kohli") {
 
-            getPlayerStats(player?.cricviz_id).then((player_stats) => {
-                getPlayerMatchStats(player_stats)
-                // fs.writeFile(`${player.symbol}.json`, JSON.stringify(player_stats), (err) => {
-                //     if (err) {
-                //         console.error('Error writing JSON file:', err);
-                //     } else {
-                //         console.log('JSON file has been written successfully.');
-                //     }
-
-
-            });
-
-
-
-            // })
+      getCrickvizPlayer(player.symbol, player.cricviz_id).then(async (player_stats) => {
+        const teams = player_stats.data.sixergame_sixers[0].allTeams;
+        const teamIds = []; // Array to store the team IDs
+        // console.log(teams)
+        for (const team of teams) {
+          const teamId = await getTeamIdByCode(team);
+          teamIds.push(teamId);
         }
-        // Access other keys as needed
-    });
+
+        let player_info = {
+          "Player": playerKey,
+          "Name": player.name,
+          "Player Type": player.playerType,
+          "Tournaments": player.tournaments,
+          "Jersy Number": player.jersey_number,
+          "Sixer ID": player.sixer_id,
+          "Cricviz ID": player.cricviz_id,
+          "Sport ID": player.sportId,
+          "Symbol": player.symbol,
+        }
+        console.log(player_info)
+
+        // Insert player_info into Hasura table
+        insertData({
+          cricviz_id:player.cricviz_id, jersy_number:player.jersey_number, name:player.name, player_type:player.playerType, sixer_id:parseInt(player.sixer_id), sport_id:player.sportId, symbol:player.symbol
+        });
+
+
+
+      })
+
+      // getPlayerStats(player?.cricviz_id).then((player_stats) => {
+      //   getPlayerMatchStats(player_stats)
+      //   // fs.writeFile(`${player.symbol}.json`, JSON.stringify(player_stats), (err) => {
+      //   //     if (err) {
+      //   //         console.error('Error writing JSON file:', err);
+      //   //     } else {
+      //   //         console.log('JSON file has been written successfully.');
+      //   //     }
+
+
+      // });
+
+
+
+      // })
+    // }
+    // Access other keys as needed
+  });
 }
 getPlayerData()
 
-const getMatchStats = require('./Match.stats');
-const { calculateBattingPoints, calculateFieldingPoints } = require('./Points.table');
-const getPlayerPoints = require('./Points.table');
-const { Console } = require('console');
 
 function getPlayerMatchStats(player_stats) {
-    player_stats?.data?.datascience_cricviz_player_match.forEach(match => {
-        // console.log("Start Time:", match.start_timestamp);
-        // console.log("Match Id:", match.match_scoring.sixer_player_match_id);
-        // console.log("Start Time:", match.start_timestamp);
-        getMatchStats(match?.match_scoring?.sixer_player_match_id).then((player_stats) => {
+  player_stats?.data?.datascience_cricviz_player_match.forEach(match => {
+    // console.log("Start Time:", match.start_timestamp);
+    // console.log("Match Id:", match.match_scoring.sixer_player_match_id);
+    // console.log("Start Time:", match.start_timestamp);
+    getMatchStats(match?.match_scoring?.sixer_player_match_id).then((player_stats) => {
 
-            // fs.writeFile(`${match.match_scoring.match}.json`, JSON.stringify(player_stats), (err) => {
-            //     if (err) {
-            //         console.error('Error writing JSON file:', err);
-            //     } else {
-            //         console.log('JSON file has been written successfully.');
-            //     }
-            // });
+      // fs.writeFile(`${match.match_scoring.match}.json`, JSON.stringify(player_stats), (err) => {
+      //     if (err) {
+      //         console.error('Error writing JSON file:', err);
+      //     } else {
+      //         console.log('JSON file has been written successfully.');
+      //     }
+      // });
 
-            player_stats?.data?.datascience_cricviz_match_scoring.forEach(element => {
-                let player_current_performance = {
-                "bat_runs": player_stats.data.datascience_cricviz_match_scoring[0].bat_runs,
-                "balls_faced": player_stats.data.datascience_cricviz_match_scoring[0].balls_faced,
-                "fours_hit": player_stats.data.datascience_cricviz_match_scoring[0].fours_hit,
-                "sixes_hit": player_stats.data.datascience_cricviz_match_scoring[0].sixes_hit,
-                "bowl_wickets": player_stats.data.datascience_cricviz_match_scoring[0].bowl_wickets,
-                "balls_bowled": player_stats.data.datascience_cricviz_match_scoring[0].balls_bowled,
-                "bowl_runs": player_stats.data.datascience_cricviz_match_scoring[0].bowl_runs,
-                "dot_balls": player_stats.data.datascience_cricviz_match_scoring[0].dot_balls,
-                "maidens": player_stats.data.datascience_cricviz_match_scoring[0].maidens,
-                "catch": player_stats.data.datascience_cricviz_match_scoring[0].catch,
-                "stumping": player_stats.data.datascience_cricviz_match_scoring[0].stumping,
-                "run_out": player_stats.data.datascience_cricviz_match_scoring[0].run_out,
-                "run_points": player_stats.data.datascience_cricviz_match_scoring[0].run_points,
-                "milestone_bonus": player_stats.data.datascience_cricviz_match_scoring[0].milestone_bonus,
-                "batting_participation_points": player_stats.data.datascience_cricviz_match_scoring[0].batting_participation_points,
-                "batting_multiplier": player_stats.data.datascience_cricviz_match_scoring[0].batting_multiplier,
-                "match": match.match_scoring.match,
-                "Date": match.start_timestamp
-            };
+      player_stats?.data?.datascience_cricviz_match_scoring.forEach(element => {
+        let player_current_performance = {
+          "bat_runs": player_stats.data.datascience_cricviz_match_scoring[0].bat_runs,
+          "balls_faced": player_stats.data.datascience_cricviz_match_scoring[0].balls_faced,
+          "fours_hit": player_stats.data.datascience_cricviz_match_scoring[0].fours_hit,
+          "sixes_hit": player_stats.data.datascience_cricviz_match_scoring[0].sixes_hit,
+          "bowl_wickets": player_stats.data.datascience_cricviz_match_scoring[0].bowl_wickets,
+          "balls_bowled": player_stats.data.datascience_cricviz_match_scoring[0].balls_bowled,
+          "bowl_runs": player_stats.data.datascience_cricviz_match_scoring[0].bowl_runs,
+          "dot_balls": player_stats.data.datascience_cricviz_match_scoring[0].dot_balls,
+          "maidens": player_stats.data.datascience_cricviz_match_scoring[0].maidens,
+          "catch": player_stats.data.datascience_cricviz_match_scoring[0].catch,
+          "stumping": player_stats.data.datascience_cricviz_match_scoring[0].stumping,
+          "run_out": player_stats.data.datascience_cricviz_match_scoring[0].run_out,
+          "run_points": player_stats.data.datascience_cricviz_match_scoring[0].run_points,
+          "milestone_bonus": player_stats.data.datascience_cricviz_match_scoring[0].milestone_bonus,
+          "batting_participation_points": player_stats.data.datascience_cricviz_match_scoring[0].batting_participation_points,
+          "batting_multiplier": player_stats.data.datascience_cricviz_match_scoring[0].batting_multiplier,
+          "match": match.match_scoring.match,
+          "Date": match.start_timestamp
+        };
 
-            console.log("\nPLAYER Performace\n")
-            console.log(player_current_performance)
+        console.log("\nPLAYER Performace\n")
+        console.log(player_current_performance)
 
-            console.log("\nPLAYER POINTS\n")
-            getPlayerPoints(player_current_performance)
-            });
+        console.log("\nPLAYER POINTS\n")
+        getPlayerPoints(player_current_performance)
+      });
 
-            
 
-        })
-    });
+
+    })
+  });
 }
 // getPlayerMatchStats()
 
 
 
-// Function to insert player info into Hasura table
-async function insertPlayerInfo(playerInfo) {
-  const query = `
-    mutation InsertPlayerInfo($info: player_info_insert_input!) {
-      insert_player_info_one(object: $info) {
-        id
-      }
-    }
-  `;
-  const variables = {
-    info: playerInfo
-  };
 
-  try {
-    const response = await fetch(YOUR_API_ENDPOINT, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        query,
-        variables
-      }),
-    });
 
-    const data = await response.json();
-    console.log('Player info inserted:', data);
-  } catch (error) {
-    console.error('Error inserting player info:', error);
-  }
-}
+
