84280ecff2cfa850882bd80586c44dfebd45a49e
Madhav008 <madhavj211@gmail.com>
2023-05-27 22:43:57 +0530

    Price Calculation Done

---
+++ Server/PlayerPrice/PlayerPriceFormula.js
@@ -0,0 +1,27 @@
+
+
+function calculatePlayerOldPrice(){
+    return 50;
+}
+
+function PriceChangeFactor(MatchPoints,Performance_avg){
+    /* 
+        MatchPoints = Total Points that are earned by the player in the single match
+        Performance_avg = Average points of 30 matches
+    */
+
+    let price_differntial  = 1 + 0.14*((MatchPoints-Performance_avg)/((0.5*MatchPoints)+Performance_avg));
+
+    let price_change_factor = 0.005 + 1/(2-price_differntial);
+
+    return price_change_factor;
+
+}
+
+function calculatePlayerPrice(){
+    /* Price New = Price OLD  + Price Change Factor */
+
+    let new_price = PriceChangeFactor(MatchPoints,Performance_avg)+calculatePlayerOldPrice();
+
+    return new_price;
+}
