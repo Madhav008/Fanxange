1d49c511f2d4560c3ee3adb9fde37a0232fcc6b9
Madhav008 <madhavj211@gmail.com>
2023-07-01 18:38:45 +0530

    Dummy Data added

--- frontend/fanxange-react/src/Components/TradeHistory.jsx
+++ frontend/fanxange-react/src/Components/TradeHistory.jsx
@@ -9,12 +9,12 @@ const TradeHistory = () => {
             <div className='mx-5 h-full overflow-y-scroll'>
             
 
-            <div className="overflow-x-auto text-white h-[450px]">
-                <table className="table">
+            <div className="overflow-x-auto text-white h-[450px] ">
+                <table className="table ">
                     {/* head */}
                     <thead>
                         <tr>
-                            <th></th>
+                        
                             <th className='text-white'>Price</th>
                             <th className='text-white'>VOLUME</th>
                             <th className='text-white'>TIME</th>
@@ -23,24 +23,21 @@ const TradeHistory = () => {
                     <tbody>
                         {/* row 1 */}
                         <tr>
-                            <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
+                            <td className='text-red-400'>26,52,001. 80</td>
+                            <td>0.03770</td>
+                            <td>18:35:19</td>
                         </tr>
                         {/* row 2 */}
                         <tr>
-                            <th>2</th>
-                            <td>Hart Hagerty</td>
-                            <td>Desktop Support Technician</td>
-                            <td>Purple</td>
+                            <td className='text-green-400'>26,52,001. 80</td>
+                            <td>0.03770</td>
+                            <td>18:35:19</td>
                         </tr>
                         {/* row 3 */}
                         <tr>
-                            <th>3</th>
-                            <td>Brice Swyre</td>
-                            <td>Tax Accountant</td>
-                            <td>Red</td>
+                            <td className='text-red-400'>26,52,001. 80</td>
+                            <td>0.03770</td>
+                            <td>18:35:19</td>
                         </tr>
                     </tbody>
                 </table>
