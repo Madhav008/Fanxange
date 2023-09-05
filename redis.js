95700bd430086e117d4254685301116ececb7cc9
Madhav008 <madhavj211@gmail.com>
2023-09-05 14:34:42 +0530

    redis-config

--- redis.js
+++ redis.js
@@ -3,10 +3,19 @@ const redis = require('redis');
 
 // Initialize Redis client
 const redisClient = redis.createClient({
-  host: 'localhost', // Replace with your Redis server host
+  host: '192.168.1.124', // Replace with your Redis server host
   port: 6379,        // Replace with your Redis server port
 });
 
-redisClient.connect();
+(async () => {
+  await redisClient.connect();
+})();
 
+redisClient.on('connect', function () {
+  console.log('Connected!');
+});
+
+redisClient.on('error', (err) => {
+  console.error(err);
+});
 module.exports = redisClient;
