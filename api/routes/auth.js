e3824eecb0bcb783b5840a7667e00149046cce3e
Madhav008 <madhavj211@gmail.com>
2023-10-02 01:05:17 +0530

    auth routes added

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
 const authController = require('../controller/authController');
 require('dotenv').config();
 
@@ -10,16 +9,15 @@ router.get('/google/login', authController.googleLogin);
 // Success endpoint
 router.get('/success', authController.successEndpoint);
 
-// Failure endpoint
-router.get('/failure', authController.failureEndpoint);
 
+router.get('/google/callback',authController.googleCallback)
 
-router.get('/google/callback',
+/* router.get('/google/callback',
     passport.authenticate('google', { failureRedirect: '/failed' }),
     function (req, res) {
         res.redirect(process.env.CLIENT_URL);
     });
-
+ */
 router.get('/logout', function (req, res) {
     req.logout(function (err) {
         if (err) { return next(err); }
