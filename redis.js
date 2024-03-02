9138772cc469347ca40fd2e9e13ccd99ed2f9392
Madhav008 <madhavj211@gmail.com>
2024-03-02 19:36:17 +0530

    redis updated

--- redis.js
+++ redis.js
@@ -1,20 +1,17 @@
-import redis from 'redis';
-import { logWithInfo, logger } from './logger.js';
+const redis = require('redis')
 
 // Initialize Redis client
 const redisClient = redis.createClient({
-    host: '192.168.1.124',
-    port: 6379,
+  host: '192.168.1.124',
+  port: 6379,
 });
 
 redisClient.on('connect', function () {
-    console.log('Connected!');
-    logWithInfo({ message: 'Redis Connected' });
+  console.log('Redis Connected!');
 });
 
 redisClient.on('error', (err) => {
-    console.error(err);
-    logger.error({ message: err });
+  console.error(err);
 });
 
-export { redisClient };
+module.exports = { redisClient };
